4096fdbdbb7b50924e9d36e805faeb3cd6b319ca
Alexis Coulombe <acoulombe@globalia.ca>
2019-06-19 15:57:04 -0400

    Added form to add database info to api

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -4,12 +4,29 @@
 
 @section('content')
     <div id="content">
-        <h2 class="style5">Add api!</h2>
+        <h2 class="style5">Add api</h2>
 
         <form action="/api/add" method="POST">
             @csrf
             <label for="api_name">Api name</label>
             <input name="api_name" placeholder="Name" required/><br />
+
+            <input type="submit" />
+        </form>
+
+        <h2 class="style5">Set database connection to this api</h2>
+
+        <form action="" method="POST">
+            @csrf
+            <label for="database_host">Host</label>
+            <input type="text" name="database_host" placeholder="127.0.0.1" required/><br />
+            <label for="database_username">Username</label>
+            <input type="text" name="database_username" placeholder="admin" required/><br />
+            <label for="database_password">Password</label>
+            <input type="password" name="database_password" placeholder="password" required/><br />
+            <label for="database_charset">Charset</label>
+            <input type="text" name="database_charset" placeholder="utf8" required/><br />
+
             <input type="submit" />
         </form>
 
